4aa580f439a494fc5f2efef218ba06e90d92b68a
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-07-14 21:54:40 +0530

    Update map.py

--- map.py
+++ map.py
@@ -17,7 +17,7 @@ time.sleep(3)
 driver.get("https://www.google.com/maps/@12.9531904,77.6142848,12z?entry=ttu")
 time.sleep(3)
 search_box = driver.find_element_by_name("q")
-search_box.send_keys("sarvodaya boys school tumkur") #replace with which you want to do ratings
+search_box.send_keys("Oxford, MS, America") #replace with which you want to do ratings
 search_box.send_keys(Keys.RETURN)
 time.sleep(6)
 driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div:nth-child(3) > div > div > button:nth-child(2) > div.LRkQ2 > div.Gpq6kf.fontTitleSmall").click() #click on review button 
