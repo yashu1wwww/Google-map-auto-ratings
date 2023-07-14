bbf0b07a32ce2a6e88f35793a3f6df4465e33cb5
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-07-14 21:53:45 +0530

    Update map.py

--- map.py
+++ map.py
@@ -1,84 +1,36 @@
-import os
-import time
-import config
-import numpy as np
-import spintax
-import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from random import randint, randrange, random
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager as CM
-from lxml.html import fromstring
-import requests
-from itertools import cycle
-import json
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 import random
 
-def login(email, password, proxy=None):
-
-    options = uc.ChromeOptions()
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-extensions")
-    if proxy is not None:
-        options.add_argument('--proxy-server=%s' % proxy)
-
-    driver = uc.Chrome(options=options, executable_path=CM().install(),use_subprocess=True)
-    driver.get('https://accounts.google.com/v3/signin/identifier?dsh=S-921757013%3A1687279134332255&continue=https%3A%2F%2Fwww.google.com%2Fmaps%2Fapi%2Fjs%2FReviewsService.CrossOriginAuthSuccess%3Fpb%3D%211shttps%253A%252F%252Fwww.google.com&hl=en&ifkv=Af_xneHefyDeZ1e-F6Dd2wIVUx4xfs5BV7hpPmSVEyNOoXkZ8PbI5xBYKNFi47JLLP-AzcLJX15APg&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-
-    wait=WebDriverWait(driver, 50);
-    email_field=wait.until(EC.visibility_of_element_located((By.ID,'identifierId')))
-    for char in email:
-        email_field.send_keys(char)
-
-    wait.until(EC.visibility_of_element_located((By.ID,"identifierNext"))).click()
-    time.sleep(3)
-    password_field=wait.until(EC.visibility_of_element_located((By.NAME,'Passwd')))
-    for char in password:
-        password_field.send_keys(char)
-    wait.until(EC.visibility_of_element_located((By.ID,"passwordNext"))).click()
-    
-    time.sleep(3)#when accounts auto login if ask otp means change sleep into 15 seconds  
-    
-    with open("urls.txt") as f:
-         for url in f:
-             driver.get(url) 
-             
-    time.sleep(7)
-    
-    time.sleep(7)
-    driver.find_element_by_xpath('/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[4]/div/button/span/div').click()
-    #driver.implicitly_wait(7)
-    time.sleep(2)
-    driver.find_element_by_xpath('/html/body/div/c-wiz/div/div/div/c-wiz/div/div[1]/div[3]/div/div[2]/div/div[5]/svg').click()
-    time.sleep(5)
-    driver.find_element_by_xpath('/html/body/div/c-wiz/div/div/div/c-wiz/div/div[1]/div[3]/div/div[3]/div[1]/div/label').send_keys("some text")
-    #driver.find_element_by_css_selector('#kCvOeb > div.O51MUd > div.C1FYvf > div > div.G9Xzm > div.RAKQ3e.RNlq > div > label').send_keys("some text")
-    time.sleep(5)
-    driver.find_element_by_xpath('/html/body/div/c-wiz/div/div/div/c-wiz/div/div[2]/div/div[2]/div/button/div[1]').click()
-    driver.find_element_by_css_selector('#kCvOeb > div.bTLhlf > div > div.kEocrb > div > button > div.VfPpkd-Jh9lGc').click()
-    driver.close()#after one acc auto login and hits auto google ratings it again choose another accs and do same process
-
-
-def main(email,password):
-    driver = login(email, password)
-    wait = WebDriverWait(driver, 50);
-    keywords = [];
-    
-        
-       
-
-if __name__ == '__main__':
-    with open('email.txt', 'r', encoding="utf8") as f:
-        keywords = [line.strip() for line in f]
-        for user in keywords:
-            email_pass = user.split(",")
-            main(email_pass[0],email_pass[1])
-
+option = webdriver.ChromeOptions()
+option.add_experimental_option("debuggerAddress", "localhost:8080")
+driver = webdriver.Chrome(options=option)
+time.sleep(3)
+driver.get("https://www.google.com/maps/@12.9531904,77.6142848,12z?entry=ttu")
+time.sleep(3)
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("sarvodaya boys school tumkur") #replace with which you want to do ratings
+search_box.send_keys(Keys.RETURN)
+time.sleep(6)
+driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div:nth-child(3) > div > div > button:nth-child(2) > div.LRkQ2 > div.Gpq6kf.fontTitleSmall").click() #click on review button 
+time.sleep(4)
+driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div.m6QErb.Hk4XGb.QoaCgb.KoSBEe.tLjsW > div > button > span > span").click() #click on write a review
+time.sleep(5)
+driver.maximize_window()
+time.sleep(4)
+driver.find_element(By.CSS_SELECTOR, "#kCvOeb > div.O51MUd > div.C1FYvf > div > div.HDUCif > div > div.Rgwf9b > span.jwzVle > span > div.VfPpkd-dgl2Hf-ppHlrf-sM5MNb > button > span.VfPpkd-kBDsod > svg").click() #click on posting publicity icon for bypass msg
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="kCvOeb"]/div[1]/div[3]/div/div[2]/div/div[5]/svg').click()#click on 5th star
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="c2"]').send_keys("amazing school with good education system") #send review comment
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR "#kCvOeb > div.bTLhlf > div > div.kEocrb > div > button > div.VfPpkd-RLmnJb").click() #click on post button
+time.sleep(4)
