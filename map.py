711b698313dbe68c2a056cced9b9c6cbb53a21f9
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-08-15 18:43:17 +0530

    Update map.py

--- map.py
+++ map.py
@@ -29,5 +29,12 @@ time.sleep(4)
 
 driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div.m6QErb.Hk4XGb.QoaCgb.KoSBEe.tLjsW > div > button > span > span").click() #click on write a review
 
-#After clicking on the "Write a review" button, the star option appears, but it appears in medium size. I've tried all possible methods to avoid clicking the star button. If this issue occurs, I will add the remaining part of the code. Alternatively, if you are familiar with it, you can fork the repository.
+#After clicking on the "Write a review" button, the star option appears, but it appears in medium size. I've tried all possible methods to avoid clicking the star button. If this issue occurs, I will add the remaining part of the code. Alternatively, if you are familiar with it, you can fork the repository below is the code...
 
+#The Star button and review text code
+
+WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[18]/iframe")))
+self.driver.find_element(By.XPATH,("/html/body/div[1]/c-wiz/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/textarea")).send_keys("Comment")
+elem=WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#yDmH0d > c-wiz > div > div > div > div > div.O51MUd > div.l5dc7b > div.DTDhxc.eqAW0b > div.euWHWd.aUVumf > div > div:nth-child(5)")))
+self.driver.execute_script("arguments[0].click();", elem)
+WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#ZRGZAf > span"))).click()
