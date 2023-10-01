0a5d6de226e75b29b83d145713d0b2ac66082521
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-10-01 12:42:25 +0530

    Update map.py

--- map.py
+++ map.py
@@ -13,14 +13,15 @@ import random
 import os
 
 option = webdriver.ChromeOptions()
-option.add_experimental_option("debuggerAddress", "localhost:8080")
+option.add_experimental_option("debuggerAddress", "localhost:9222")
 driver = webdriver.Chrome(options=option)
-time.sleep(3)
+time.sleep(4)
 driver.get("https://www.google.com/maps/@12.9531904,77.6142848,12z?entry=ttu")
-time.sleep(3)
+time.sleep(4)
 search_box = driver.find_element(By.NAME, "q")
-search_box.send_keys("sarvodaya boys school") #replace with your search place
+search_box.send_keys("oxford school melbourne") #replace with your search place
 search_box.send_keys(Keys.RETURN)
+
 time.sleep(6)
 
 driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div:nth-child(3) > div > div > button:nth-child(2) > div.LRkQ2 > div.Gpq6kf.fontTitleSmall").click() #click on review button 
@@ -29,12 +30,16 @@ time.sleep(4)
 
 driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div.m6QErb.Hk4XGb.QoaCgb.KoSBEe.tLjsW > div > button > span > span").click() #click on write a review
 
-#After clicking on the "Write a review" button, the star option appears, but it appears in medium size. I've tried all possible methods to avoid clicking the star button. If this issue occurs, I will add the remaining part of the code. Alternatively, if you are familiar with it, you can fork the repository below is the code...
+time.sleep(6)
 
-#The Star button and review text code
+driver.find_element_by_css_selector("#yDmH0d > c-wiz > div > div > div > div > div.O51MUd > div.l5dc7b > div.DTDhxc.eqAW0b > div.euWHWd.aUVumf > div > div:nth-child(5)").click() #click on rating button 
 
-WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[18]/iframe")))
-self.driver.find_element(By.XPATH,("/html/body/div[1]/c-wiz/div/div/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/textarea")).send_keys("Comment")
-elem=WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#yDmH0d > c-wiz > div > div > div > div > div.O51MUd > div.l5dc7b > div.DTDhxc.eqAW0b > div.euWHWd.aUVumf > div > div:nth-child(5)")))
-self.driver.execute_script("arguments[0].click();", elem)
-WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#ZRGZAf > span"))).click()
+time.sleep(3)
+
+driver.find_element_by_xpath('/html/body/div/c-wiz/div/div/div/c-wiz/div/div[1]/div[3]/div/div[3]/div[1]/div/label/textarea').send_keys("one of the best school") #comment about it
+
+time.sleep(2)
+
+driver.find_element_by_xpath('/html/body/div/c-wiz/div/div/div/c-wiz/div/div[2]/div/div[2]/div/button/div[1]').click() #click on post button
+
+time.sleep(10)
