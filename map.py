4aad956ba5f7ed9992119fb176c449190f5848b4
Yashwanth R <89150253+yashu1wwww@users.noreply.github.com>
2025-12-06 12:45:04 +0530

    Update map.py

--- map.py
+++ map.py
@@ -22,17 +22,21 @@ search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("Sarvodaya PU College, Tumakuru-1, Horpet, Ward No. 18, Tumkur, Tumakuru, Karnataka") #replace with your search place
 search_box.send_keys(Keys.RETURN)
 
-time.sleep(6)
-
-driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div:nth-child(3) > div > div > button:nth-child(2) > div.LRkQ2 > div.Gpq6kf.fontTitleSmall").click() #click on review button 
-
 time.sleep(4)
 
-driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf > div.m6QErb.Hk4XGb.QoaCgb.KoSBEe.tLjsW > div > button > span > span").click() #click on write a review
+driver.find_element_by_xpath("//div[text()='Reviews']").click()
+
+time.sleep(3)
+
+driver.find_element_by_xpath("//span[text()='Write a review']").click()
+
+time.sleep(2)
+
+driver.find_element_by_xpath("/html/body/div/c-wiz/div/div/div/c-wiz/div").click()
 
 WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[6]/iframe")))
 
-time.sleep(2)
+time.sleep(5)
 
 driver.find_element_by_css_selector("#kCvOeb > div.O51MUd > div.C1FYvf > div.NUTAPc.CbHt0b > div.HDUCif > div > div.Rgwf9b > span.jwzVle > span > div.VfPpkd-dgl2Hf-ppHlrf-sM5MNb > button > span.VfPpkd-kBDsod").click() #if popup appears...
 
